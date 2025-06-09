f28cb3717b834111cfb9bb1a0a01a1598667488c
Pantha Acharjee <freelancerpantha@gmail.com>
2025-06-09 17:10:34 +0600

    First commit

--- index.ts
+++ index.ts
@@ -18,18 +18,4 @@ conncetDB()
 server.listen(process.env.PORT,()=>console.log(`Server is working on http://localhost:${process.env.PORT}`))
 
 
-//Unhandled Promise Rejection Error
-process.on("unhandledRejection", (err:any) => {
-  console.log(`Error: ${err.message}`);
-  console.log("Shutting down the server due to Unhandled Promise Rejection");
-  process.exit(1);
-});
-
-
-//Uncaught Error
-process.on("uncaughtException", (err:any) => {
-  console.log(`Error: ${err.message}`);
-  console.log("Shutting down the server due to Unhandled Promise Rejection");
-  process.exit(1);
-});
 
